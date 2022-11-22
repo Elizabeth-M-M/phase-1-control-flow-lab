dadbf8e2e5a5d5de38f014c1426d0d541c24954e
Elizabeth <mwendemuthusi6@gmail.com>
2022-11-22 11:39:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -13,26 +13,25 @@ function scuberGreetingForFeet(ride){
 
 function ternaryCheckCity(city){
   // Write your code here!
-  if (city === 'NYC') {
-    return 'Ok, sounds good.';
-  }  else{
-    return "No go.";
-  }
+  return city === 'NYC'?'Ok, sounds good.':
+   "No go.";
 
 }
 
-function switchOnCharmFromTip(tip){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-  if (tip === 'generous') {
-  return 'Thank you so much.'
-} else if (tip === 'not as generous') {
-  return 'Thank you.'
-} else{
-    return 'Bye.';
-}
-
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break
+    case 'not as generous':
+      return 'Thank you.';
+      break
+    default:
+      return 'Bye.';
+  }
 }
 
 console.log(scuberGreetingForFeet(300));
-console.log(ternaryCheckCity('Pittsburgh'));
-console.log(switchOnCharmFromTip('generous'));
+console.log(ternaryCheckCity('Buffalo'));
+console.log(switchOnCharmFromTip('hey'));
